e37d2c0e173f2a36cf9aa467ecc9b488fbec9897
Anthony Wingfield <awiiing@Anthonys-MacBook-Pro.local>
2021-02-09 11:38:06 -0800

    init supabase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { Auth } from "@supabase/ui";
+import { supabase } from "../lib/initSupabase";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+    </Auth.UserContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
